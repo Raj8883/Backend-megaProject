74c5d20b63b5ae2c581a7e770497836f47230a07
Raj Patel <rajkumar2004patel@gmail.com>
2025-05-28 12:15:41 +0530

    utils code update

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+
+
+
+
+
+dotenv.config({
+    path:'./env'
+});
+
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT||8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error => {
+    console.error('Error connecting to MongoDB:', error);
+}));
+
+
+
+
+// ;(async ()=>{
+
+//     try {
+//          await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.error('Error connecting to MongoDB:', error);
+//         throw error;
+//     }
+// })()
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+const asyncHandler = (requestHandler) => {
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((error)=>next(error));
+    }
+}
+
+// const asyncHandler = (fn) => async() => {
+//     try{
+//         await fn(req, res, next)
+//     }catch(error){
+//         res.status(error.code||500).json({
+//             success: false,
+//             message: error.message || 'Internal Server Error',
+//         });
+//     }
+  
+// };
+
+export {asyncHandler};
